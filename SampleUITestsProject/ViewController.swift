b092bfd414795e64150718f9ce431c5d67cf3b65
Clement Roulland <clement.roulland@gmail.com>
2016-11-02 10:45:06 -0400

    Initial Commit

---
+++ SampleUITestsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleUITestsProject
+//
+//  Created by Clement Roulland on 16-11-02.
+//  Copyright © 2016 clement. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

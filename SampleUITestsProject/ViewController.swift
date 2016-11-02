08774c424a2446213d8c9c2f3949a0d11ca2d15e
Clement Roulland <clement.roulland@gmail.com>
2016-11-02 11:19:41 -0400

    make an API call

--- SampleUITestsProject/ViewController.swift
+++ SampleUITestsProject/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeACall()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: Business
 
-
+    func makeACall() {
+        
+        Alamofire.request("https://jsonplaceholder.typicode.com/posts").responseJSON { response in
+            
+            guard let JSON = response.result.value as? [[String: Any]],
+                let titleLabel = JSON.first?["title"] as? String else
+            {
+                self.titleLabel.text = "Failed 😢"
+                return
+            }
+            
+            self.bodyTextView.text = JSON.debugDescription
+            self.titleLabel.text = titleLabel
+        }
+    }
 }
 
